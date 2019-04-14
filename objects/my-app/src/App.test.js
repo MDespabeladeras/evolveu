7145fa05423d6560531b61046df9b9b40701018b
Marilou Despabeladeras <mdespabeladeras@gmail.com>
2019-04-14 11:27:59 -0600

    fixed folder structure

--- objects/my-app/src/App.test.js
+++ objects/my-app/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MathComp from "./MathComp";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
