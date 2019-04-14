cc32079dc94981a8a5c9c8cd0bd9261bec287e39
Marilou Despabeladeras <mdespabeladeras@gmail.com>
2019-04-14 10:34:43 -0600

    first commit

---
+++ objects/my-app/src/components/src/math.js
@@ -0,0 +1,36 @@
+//sum test
+//function sum(a, b) {
+//	let sum = a + b;
+//	return sum;
+//}
+//export default { sum };
+
+
+//subtract test
+//function subtract(a, b) {
+	//let subtract = a - b;
+	//return subtract;
+//}
+//export default { subtract };
+
+
+//multiply test
+//function multiply(a, b) {
+	//let multiply = a * b;
+	//return multiply;
+//}
+//export default { multiply };
+
+
+//divide test
+function divide(a, b) {
+	let divide = a / b;
+	return divide;
+}
+export default { divide };
+
+
+
+
+
+
---
+++ objects/my-app/src/components/src/math.test.js
@@ -0,0 +1,29 @@
+import math from "./math";
+
+
+
+//test ('Test the math functions', () => {
+	//console.log('Hello World');
+	//expect(math.sum(1,2)).toBe(3);
+//});
+
+
+
+//test ('Test the math functions', () => {
+	//console.log('Hello World');
+	//expect(math.subtract(2,1)).toBe(1);
+//});
+
+
+
+//test ('Test the math functions', () => {
+	//console.log('Hello World');
+	//expect(math.multiply(2,3)).toBe(6);
+//});
+
+
+
+test ('Test the math functions', () => {
+	console.log('Hello World');
+	expect(math.divide(6,3)).toBe(2);
+});
